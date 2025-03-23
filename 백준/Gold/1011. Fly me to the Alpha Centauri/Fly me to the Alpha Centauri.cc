280f1920763dded842d1a6a14f47d15d2dd1a6ff
dudongtak <82008463+dudongtak@users.noreply.github.com>
2025-03-23 15:15:58 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int t,s,e;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin>>t;
+
+    while(t>0){
+        t--;
+        cin>>s>>e;
+
+        int cnt=0;
+
+        int size=e-s;
+        int s=sqrt(size);
+        int m=(pow(s,2)+pow(s+1,2))/2;
+        if(s*s==size){
+            cout<<s*2-1<<"\n";
+        }
+        else if(m>=size){
+            cout<<s*2<<"\n";
+        }
+        else{
+            cout<<s*2+1<<"\n";
+        }
+    }
+}
