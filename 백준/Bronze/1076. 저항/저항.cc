4dce67fda502d5b2e8d0751632c0b65123893e47
dudongtak <82008463+dudongtak@users.noreply.github.com>
2025-04-30 17:58:58 +0900

    [Bronze II] Title: 저항, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.cc
+++ 백준/Bronze/1076. 저항/저항.cc
@@ -4,26 +4,27 @@
 #include<math.h>
 using namespace std;
 
-typedef pair<int, int> pii;
-map<string, pair<int, int>> m;
+typedef pair<char, int> pci;
+map<string, pair<char, int>> m;
 string str[] = { "black","brown","red","orange","yellow","green","blue","violet","grey","white" };
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	pii p;
+	pci p;
 	for (int i = 0; i < 10; i++) {
 		int n = pow(10, i);
-		p = { i,n };
+		p = { i + '0',n };
 		m[str[i]] = p;
 	}
 	string ans = "";
+
 	long long answer;
 	for (int i = 0; i < 3; i++) {
 		string tmp;
 		cin >> tmp;
-		if (i != 2)ans += to_string(m[tmp].first);
+		if (i != 2)ans += m[tmp].first;
 		else {
 			answer = stoll(ans)*m[tmp].second;
 		}
