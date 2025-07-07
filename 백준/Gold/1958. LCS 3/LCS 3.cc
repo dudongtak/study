610dc68935a86ada7d27e8b89c3ee9c311fd0e6f
dudongtak <82008463+dudongtak@users.noreply.github.com>
2025-07-07 11:55:36 +0900

    [Gold IV] Title: LCS 3, Time: 4 ms, Memory: 6512 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+using namespace std;
+
+string s1,s2,s3;
+int answer=0;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin>>s1>>s2>>s3;
+    vector<vector<vector<int>>> dp(s1.size()+1,vector<vector<int>>(s2.size()+1,vector<int>(s3.size()+1,0)));
+
+    for(int i=1;i<=s1.size();i++){
+        for(int j=1;j<=s2.size();j++){
+            for(int k=1;k<=s3.size();k++){
+                if(s1[i-1]==s2[j-1]&&s2[j-1]==s3[k-1]){
+                    dp[i][j][k]=dp[i-1][j-1][k-1]+1;
+                    answer=max(answer,dp[i][j][k]);
+                }
+                else dp[i][j][k]=max(dp[i][j][k-1],max(dp[i-1][j][k],dp[i][j-1][k]));
+            }
+        }
+    }
+
+    cout<<answer;
+}
