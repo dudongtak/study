3b44f6d832b6eeb97645bd4067e2756c4c459d2f
dudongtak <82008463+dudongtak@users.noreply.github.com>
2025-10-10 16:58:00 +0900

    [Gold IV] Title: 행성 연결, Time: 172 ms, Memory: 15328 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.cc
+++ 백준/Gold/16398. 행성 연결/행성 연결.cc
@@ -1,10 +1,32 @@
 #include<iostream>
-#include<queue>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 int N;
 vector<vector<int>> board;
+vector<int> arr;
+typedef struct info {
+	int dis, x, y;
+}info;
+
+bool cmp(const info &a, const info &b) {
+	return a.dis < b.dis;
+}
+vector<info> in;
+
+int Find(int a) {
+	if (a == arr[a])return a;
+	return arr[a] = Find(arr[a]);
+}
+
+void Union(int a, int b) {
+	int A = Find(a);
+	int B = Find(b);
+	if (A == B)return;
+	if (A < B)arr[B] = A;
+	else arr[A] = B;
+}
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -14,26 +36,17 @@ int main() {
 	board.resize(N, vector<int>(N));
 
 	for (int i = 0; i < N; i++)for (int j = 0; j < N; j++)cin >> board[i][j];
+	for (int i = 0; i < N; i++)arr.push_back(i);
+
+	for (int i = 0; i < N; i++)for (int j = i + 1; j < N; j++)in.push_back({ board[i][j],i,j });
+	sort(in.begin(), in.end(), cmp);
 
 	long long answer = 0;
 
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-	vector<bool> visit(N, false);
-	pq.push({ 0,0 });
-
-	while (!pq.empty()) {
-		int cur = pq.top().second;
-		int w = pq.top().first;
-		pq.pop();
-
-		if (visit[cur])continue;
-		answer += w;
-		visit[cur] = true;
-
-		for (int i = 0; i < N; i++) {
-			if (i == cur || visit[i])continue;
-			pq.push({ board[cur][i],i });
-		}
+	for (int i = 0; i < in.size(); i++) {
+		if (Find(in[i].x) == Find(in[i].y))continue;
+		answer += in[i].dis;
+		Union(in[i].x, in[i].y);
 	}
 
 	cout << answer;
