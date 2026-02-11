73ebd238b21bfde9c8cb4a88ee015a0114454330
dudongtak <82008463+dudongtak@users.noreply.github.com>
2026-02-11 15:00:28 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 0 ms, Memory: 3148 KB -BaekjoonHub

--- 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.cc
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.cc
@@ -1,24 +1,18 @@
 #include<iostream>
 #include<string>
-#include<vector>
-#include<algorithm>
 using namespace std;
 
-
-bool is_palindrome(string str){
-    int l=0;
-    int r=str.size()-1;
-
+int what_status(string str){
+    int l=0,r=str.size()-1;
     while(l<r){
-        if(str[l]!=str[r])return false;
-        r--;l++;
+        if(str[l]!=str[r])return 3;
+        l++;
+        r--;
     }
-    return true;
-}
-
-bool all_same(string str){
-    for(int i=1;i<str.size();i++)if(str[i]!=str[0])return false;
-    return true;
+    for(int i = 1; i < str.size(); i++){
+        if(str[i] != str[0]) return 2;
+    }
+    return 1;
 }
 
 int main(){
@@ -28,11 +22,8 @@ int main(){
     string str;
     cin>>str;
     
-    if(is_palindrome(str)){
-        if(all_same(str))cout<<-1;
-        else cout<<str.size()-1;
-    }
-    else{
-        cout<<str.size();
-    }
+    int status=what_status(str);
+    if(status==1)cout<<-1;
+    else if(status==2)cout<<str.size()-1;
+    else cout<<str.size();
 }
