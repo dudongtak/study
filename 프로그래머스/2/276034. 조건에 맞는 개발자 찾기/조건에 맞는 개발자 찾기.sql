0c9b317097afe51a46cad3fd086512be693f487b
dudongtak <82008463+dudongtak@users.noreply.github.com>
2026-02-27 14:48:40 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -3,6 +3,9 @@ select distinct
     d.email,
     d.first_name,
     d.last_name
-from developers d, skillcodes s
-where (s.name='C#' or s.name='Python')and s.code&d.skill_code=s.code
+#from developers d, skillcodes s
+#where (s.name='C#' or s.name='Python')and s.code&d.skill_code=s.code
+from developers d
+join skillcodes s
+on (s.name='C#' or s.name='Python') and s.code&d.skill_code=s.code
 order by id
