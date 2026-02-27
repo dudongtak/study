58bbb03ffa50ea3595e24778aea77b9e83c8bc4a
dudongtak <82008463+dudongtak@users.noreply.github.com>
2026-02-27 20:33:31 +0900

    [Gold V] Title: 기타 레슨, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.cc
@@ -5,46 +5,49 @@ using namespace std;
 
 int n, m;
 vector<int> study;
-int sum = 0;
-int answer = 1e9;
 
 bool check(int size) {
-	vector<int> blueray;
-	int s = 0, size_sum = 0;
-	for (int i = 0; i < n; i++) {
-		s += study[i];
-		if (s > size) {
-			s -= study[i];
-			if(s<=size)blueray.push_back(s);
-			s = study[i];
-		}
-	}
-	if (s <= size)blueray.push_back(s);
-	for (int i = 0; i < blueray.size(); i++)size_sum += blueray[i];
-	return blueray.size() <= m && size_sum == sum;
+    int count = 1;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (sum + study[i] > size) {
+            count++;
+            sum = study[i];
+        } else {
+            sum += study[i];
+        }
+    }
+    return count <= m;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	cin >> n >> m;
-	study.resize(n);
+    cin >> n >> m;
+    study.resize(n);
 
-	for (int i = 0; i < n; i++) {
-		cin >> study[i];
-		sum += study[i];
-	}
+    int left = 0, right = 0;
 
-	int s = 0, e = sum, m;
-	while (s <= e) {
-		m = (s + e) / 2;
+    for (int i = 0; i < n; i++) {
+        cin >> study[i];
+        left = max(left, study[i]);
+        right += study[i];
+    }
 
-		if (check(m)) {
-			e = m - 1;
-			answer = min(answer, m);
-		}
-		else s = m + 1;
-	}
-	cout << answer;
+    int answer = right;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (check(mid)) {
+            answer = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    cout << answer;
 }
