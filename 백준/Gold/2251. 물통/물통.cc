4c600893966b7bcea783df46dcf649751cafced3
dudongtak <82008463+dudongtak@users.noreply.github.com>
2026-03-09 16:41:52 +0900

    [Gold IV] Title: 물통, Time: 4 ms, Memory: 5628 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.cc
+++ 백준/Gold/2251. 물통/물통.cc
@@ -1,40 +1,48 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 using namespace std;
 
 int num[3];
 vector<vector<vector<bool>>> v(201,vector<vector<bool>>(201,vector<bool>(201,false)));
 vector<bool> ans(201,false);
+vector<int> cur(3,0);
 
-void dfs(int a,int b,int c){
-    if(v[a][b][c])return;
-    v[a][b][c]=true;
-    if(a==0)ans[c]=true;
-
-    int pour;
-
-    pour=min(num[1]-b,a);
-    dfs(a-pour,b+pour,c);
-    pour=min(num[2]-c,a);
-    dfs(a-pour,b,c+pour);
-
-    pour=min(num[0]-a,b);
-    dfs(a+pour,b-pour,c);
-    pour=min(num[2]-c,b);
-    dfs(a,b-pour,c+pour);
-
-    pour=min(num[0]-a,c);
-    dfs(a+pour,b,c-pour);
-    pour=min(num[1]-b,c);
-    dfs(a,b+pour,c-pour);
-}
+void move(int idx1,int idx2);
+void dfs();
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin>>num[0]>>num[1]>>num[2];
-    dfs(0,0,num[2]);
+    cur[2]=num[2];
+    dfs();
     for(int i=0;i<=num[2];i++)if(ans[i])cout<<i<<" ";
+}
+
+void move(int idx1,int idx2){
+    int tmp1=cur[idx1],tmp2=cur[idx2];
+    cur[idx2]+=cur[idx1];
+    cur[idx1]=0;
+    if(cur[idx2]>num[idx2]){
+        cur[idx1]=cur[idx2]-num[idx2];
+        cur[idx2]=num[idx2];
+    }
+    dfs();
+    cur[idx1]=tmp1;cur[idx2]=tmp2;
+}
+
+void dfs(){
+    if(v[cur[0]][cur[1]][cur[2]])return;
+    v[cur[0]][cur[1]][cur[2]]=true;
+    if(cur[0]==0)ans[cur[2]]=true;
+  
+    move(2,0);
+    move(2,1);
+
+    move(1,0);
+    move(1,2);
+
+    move(0,1);
+    move(0,2);
 }
