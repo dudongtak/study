a15c8cf5fea2eb22f0a874df77fd6f934bf2af72
dudongtak <82008463+dudongtak@users.noreply.github.com>
2026-04-07 17:02:53 +0900

    [Gold V] Title: 1학년, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n;
+vector<int> nums;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin>>n;
+    nums.resize(n);
+
+    for(int i=0;i<n;i++)cin>>nums[i];
+
+    vector<vector<long long>> dp(n,vector<long long>(21,0));
+
+    dp[0][nums[0]]=1;
+
+    for(int i=1;i<n-1;i++){
+        for(int j=0;j<=20;j++){
+            if(j-nums[i]>=0)dp[i][j-nums[i]]+=dp[i-1][j];
+        
+            if(j+nums[i]<=20)dp[i][j+nums[i]]+=dp[i-1][j];
+        }
+    }
+    cout<<dp[n-2][nums.back()];
+}
